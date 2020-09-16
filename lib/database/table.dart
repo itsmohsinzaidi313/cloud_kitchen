b165646154fcff73bac245529cc60c9f751d1083
itsmohsinzaidi <itsmohsinzaidi@users.noreply.github.com>
2020-09-16 19:50:37 +0500

    various changes

--- lib/database/table.dart
+++ lib/database/table.dart
@@ -1,15 +1,10 @@
 import 'package:food_app/shared/config.dart';
-import 'package:logger/logger.dart';
 
-class Table{
-
-  //LOGGER
-  static Logger _log = Config.log;
-
+class Table {
   //VARIABLES
   final String tableName;
   final List<String> listOfTablesColumns;
   final List<String> listOfColumnsTypes;
 
-  Table({ this.tableName, this.listOfTablesColumns, this.listOfColumnsTypes});
+  Table({this.tableName, this.listOfTablesColumns, this.listOfColumnsTypes});
 }
