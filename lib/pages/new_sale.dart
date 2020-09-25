f7d45e50511c2ac39c6292685dce23474b52c468
Muhammad Azhar <azharbhatti7437#>
2020-09-25 08:16:50 -0700

    working on new sale screen

--- lib/pages/new_sale.dart
+++ lib/pages/new_sale.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/models/objects/category.dart';
 import 'package:food_app/models/objects/item.dart';
+import 'package:food_app/models/objects/sales_detail.dart';
 import 'package:food_app/models/view_models/new_sale_model.dart';
+import 'package:food_app/shared/app_theme.dart';
+import 'package:food_app/shared/config.dart';
 
 class NewSale extends StatefulWidget {
   final NewSaleModel model;
+
   NewSale(this.model);
 
   @override
@@ -13,68 +18,65 @@ class NewSale extends StatefulWidget {
 
 class _NewSaleState extends State<NewSale> {
   final NewSaleModel model;
+
   _NewSaleState(this.model);
 
   String categoryName = '';
 
-  // GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
+  GlobalKey<ScaffoldState> _key = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // key: _key,
-      backgroundColor: Colors.white,
-      appBar: AppBar(),
+      key: _key,
+      appBar: AppTheme.appBarNormal(
+        context: context,
+        appBarTitle: 'New Sales',
+        appBarElevation: 0.0,
+        appBarBgColor: AppTheme.appBarColor,
+      ),
       body: Container(
+        color: Colors.green,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.yellowAccent,
-                shape: BoxShape.rectangle,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.white,
-                    child: GridView.count(
-                      shrinkWrap: true,
-                      crossAxisCount: 3,
-                      children: getCategoryWidgets(model.lstCategory),
+        child: Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: GridView.builder(
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: model.lstCategory.length,
+                            itemBuilder: (context, index){
+                              return Container(
+                                child: Text(
+                                  model.lstCategory[index].categoryName,
+                                ),
+                              );
+                            }
+                        ),
+                      ),
                     ),
-                  ),
-                  Container(
-                    color: Colors.white,
-                    child: GridView.count(
-                      shrinkWrap: true,
-                      crossAxisCount: 3,
-                      children: getItemsWidgets(model.lstItem, categoryName),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.yellowAccent,
-                shape: BoxShape.rectangle,
+              Expanded(
+                child: Container(
+                  color: Colors.amber,
+                ),
               ),
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.white,
-                    child: GridView.count(
-                      shrinkWrap: true,
-                      crossAxisCount: 1,
-                      children: getCartItemsWidgets(this.model.order.itemList),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -116,38 +118,38 @@ class _NewSaleState extends State<NewSale> {
   List<Widget> getItemsWidgets(List<Item> lstItem, String categoryName) {
     List<Widget> widgets = [];
     lstItem.forEach((item) {
-      if (item.categoryName == categoryName)
-        widgets.add(
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                this.model.order.addItem(item);
-              });
-            },
-            child: Card(
-              elevation: 4,
-              color: Colors.amberAccent,
-              child: Center(
-                child: Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'Ubuntu',
-                    letterSpacing: 1.0,
-                  ),
+      // if (item.categoryName == categoryName)
+      widgets.add(
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              this.model.order.addItem(item);
+            });
+          },
+          child: Card(
+            elevation: 4,
+            color: Colors.amberAccent,
+            child: Center(
+              child: Text(
+                item.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Ubuntu',
+                  letterSpacing: 1.0,
                 ),
               ),
             ),
           ),
-        );
+        ),
+      );
     });
     return widgets;
   }
 
-  List<Widget> getCartItemsWidgets(List<Item> lstItem) {  
+  List<Widget> getCartItemsWidgets(List<Item> lstItem) {
     List<Widget> widgets = [];
     lstItem.forEach((item) {
       widgets.add(
