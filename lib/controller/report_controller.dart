e04166b38c5eb7fdbaead111240c1db0671c41cc
Muhammad Azhar <azharbhatti7437#>
2020-10-29 05:52:18 -0700

    Discount field added Discount mechanism applied Report suggestion appears and in working condition

--- lib/controller/report_controller.dart
+++ lib/controller/report_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/database/table_object/sales_detail_table.dart';
 import 'package:food_app/database/table_object/sales_master_table.dart';
 import 'package:food_app/models/objects/sales_master.dart';
 import 'package:food_app/models/view_models/report_model.dart';
@@ -12,15 +13,24 @@ class ReportController{
     reportModel = ReportModel();
     reportModel.salesMaster =   SalesMaster();
     reportModel.listOfSalesMaster = [];
+    reportModel.listOfSalesDetails = [];
     getSalesMasterTable().whenComplete(() => print('Done'));
   }
 
   launch(BuildContext context) => Navigator.of(context).push(
     new MaterialPageRoute(builder: (context) => new ReportScreen(model: reportModel,)));
 
-  Future getSalesMasterTable() async{
+  static Future<List<Map<String, dynamic>>> getSalesDetailsList(int id) async{
+    List<Map<String, dynamic>> map = await Config.database
+        .rawQuery('SELECT * FROM ${SalesDetailTable.tableName} WHERE ${SalesDetailTable.salesMasterId} = $id');
+    return map;
+  }
+
+  Future getSalesMasterTable() async {
     List<Map<String, dynamic>> map = await Config.database.query(SalesMasterTable.tableName);
     map.forEach((element) => reportModel.listOfSalesMaster.add(SalesMaster.fromJson(element)));
     reportModel.listOfSalesMaster.forEach((element) => print('${element.saleNo}\n'));
   }
+
+
 }
--- lib/pages/report_screen.dart
+++ lib/pages/report_screen.dart
@@ -1,10 +1,13 @@
 import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/controller/report_controller.dart';
 import 'package:food_app/database/columns.dart';
+import 'package:food_app/models/objects/sales_detail.dart';
 import 'package:food_app/models/objects/sales_master.dart';
 import 'package:food_app/models/view_models/report_model.dart';
 import 'package:food_app/shared/app_theme.dart';
 import 'package:food_app/shared/config.dart';
+import 'package:food_app/shared/lib.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ReportScreen extends StatefulWidget {
@@ -116,56 +119,93 @@ class _ReportScreenState extends State<ReportScreen> {
             ),
             Expanded(
               flex: 5,
-              child: Container(
-                margin: EdgeInsets.only(
-                  right: 5,
-                  top: 4,
-                  bottom: 4,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Column(
-                  children: [
-                    autoCompleteTextField = AutoCompleteTextField<SalesMaster>(
-                      clearOnSubmit: false,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search Sale',
-                        contentPadding: EdgeInsets.fromLTRB(10, 30, 10, 20),
-                        hintStyle: TextStyle(color: Colors.grey),
-                      ),
-                      itemSubmitted: (item){
-                        setState(() {
-                          autoCompleteTextField.textField.controller.text = item.saleNo;
-                          print('${autoCompleteTextField.textField.controller.text}');
-                        });
-                      },
-                      key: key,
-                      suggestions: model.listOfSalesMaster,
-                      itemBuilder: (context, item){
-                        return row(item);
-                      },
-                      itemFilter: (item, query){
-                        return item.saleNo.toLowerCase().startsWith(query.toLowerCase());
-                      },
-                      itemSorter: (a,b ){
-                        return a.saleNo.compareTo(b.saleNo);
-                      },
-                    ),
-                  ],
-                ),
-              ),
+              child: getView(1),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget getView(int viewType){
+
+    switch(viewType){
+      case 1:
+        return Container(
+          margin: EdgeInsets.only(
+            right: 5,
+            top: 4,
+            bottom: 4,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: autoCompleteTextField = AutoCompleteTextField<SalesMaster>(
+                        clearOnSubmit: false,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: 'Search Sale',
+                          contentPadding: EdgeInsets.fromLTRB(10, 30, 10, 20),
+                          hintStyle: TextStyle(color: Colors.grey),
+                        ),
+                        keyboardType: TextInputType.number,
+                        itemSubmitted: (item){
+                          setState(() {
+                            model.salesMaster = item;
+                            autoCompleteTextField.textField.controller.text = item.saleNo;
+                            print('${autoCompleteTextField.textField.controller.text}');
+                          });
+                        },
+                        key: key,
+                        suggestions: model.listOfSalesMaster,
+                        itemBuilder: (context, item){
+                          return row(item);
+                        },
+                        itemFilter: (item, query){
+                          return item.saleNo.toLowerCase().startsWith(
+                              Lib.codeGenerator('ORD', int.parse(query)).toLowerCase());
+                        },
+                        itemSorter: (a, b){
+                          return a.saleNo.compareTo(b.saleNo);
+                        },
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search_rounded),
+                    color: Colors.grey,
+                    onPressed: () async{
+                      int id = int.parse(model.salesMaster.localId);
+                      ReportController.getSalesDetailsList(id).then((value) {
+                        value.forEach((element) {
+                          model.listOfSalesDetails.add(SalesDetails.fromJson(element));
+                        });
+                      }).whenComplete(() {
+                        model.listOfSalesDetails.forEach((element) => print(element.menuName));
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+        break;
+
+      default:
+        return Container();
+        break;
+    }
   }
 
   Widget row(SalesMaster item) {
